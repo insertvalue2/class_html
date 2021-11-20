514ccf14d4eba13f83cdb0fce2d2eab485b8d7a2
questzz@naver.com <questzz@naver.com>
2021-11-21 00:06:54 +0900

    add project example

---
+++ javascript/test.js
@@ -0,0 +1,17 @@
+// https://ko.javascript.info/intro
+
+
+const dog = {
+    name: '까깡',
+    job: '반려동물'
+
+};
+
+dog.name = "aaaa";
+
+console.log(dog.name);
+
+const str = "hello";
+str = "world";	//error 
+
+console.log(str);
